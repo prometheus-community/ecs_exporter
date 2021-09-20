8e0df6fd0c1b1f77690e9cdbc78f13d5722b3764
JBD <jbd@amazon.com>
2021-09-20 10:39:24 -0700

    Initial commit (#2)
    This change introduces a Prometheus exporter to publish
    ECS metrics in the Prometheus exposition format.
    
    The ecs_exporter reads the ECS metadata server to read
    ECS task and container stats. This is why
    the exporter needs to run as a sidecar container
    in an ECS task, or it should be included as a binary in
    a container deployed to be able to fetch from the
    metadata server.
    
    The exporter publishes metrics from the container runtime
    related to CPU usage, networking and more.
    Users still need to publish their custom application
    metrics from their own containers if they have any.
    
    Currently, these metrics are available from CloudWatch and
    can be ingested into Prometheus by using the cloudwatch_exporter,
    but there are various users who want to be able to directly
    export them to Prometheus to avoid delay in data collection.
    
    In the future, the exporter will report more CPU, memory,
    and I/O metrics.
    
    Fixes prometheus-community/community#36.
    
    Signed-off-by: JBD <jbd@amazon.com>

---
+++ ecscollector/collector.go
@@ -0,0 +1,216 @@
+// Copyright 2021 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ecscollector implements a Prometheus collector for Amazon ECS
+// metrics available at the ECS metadata server.
+package ecscollector
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/prometheus-community/ecs_exporter/ecsmetadata"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/tklauser/go-sysconf"
+)
+
+var (
+	cpuTotalDesc = prometheus.NewDesc(
+		"ecs_cpu_seconds_total",
+		"Total CPU usage in seconds.",
+		cpuLabels, nil)
+
+	memUsageDesc = prometheus.NewDesc(
+		"ecs_memory_bytes",
+		"Memory usage in bytes.",
+		labels, nil)
+
+	memMaxUsageDesc = prometheus.NewDesc(
+		"ecs_memory_max_bytes",
+		"Maximum memory usage in bytes.",
+		labels, nil)
+
+	memLimitDesc = prometheus.NewDesc(
+		"ecs_memory_limit_bytes",
+		"Memory limit in bytes.",
+		labels, nil)
+
+	networkRxBytesDesc = prometheus.NewDesc(
+		"ecs_network_receive_bytes_total",
+		"Network recieved in bytes.",
+		networkLabels, nil)
+
+	networkRxPacketsDesc = prometheus.NewDesc(
+		"ecs_network_receive_packets_total",
+		"Network packets recieved.",
+		networkLabels, nil)
+
+	networkRxDroppedDesc = prometheus.NewDesc(
+		"ecs_network_receive_dropped_total",
+		"Network packets dropped in recieving.",
+		networkLabels, nil)
+
+	networkRxErrorsDesc = prometheus.NewDesc(
+		"ecs_network_receive_errors_total",
+		"Network errors in recieving.",
+		networkLabels, nil)
+
+	networkTxBytesDesc = prometheus.NewDesc(
+		"ecs_network_transmit_bytes_total",
+		"Network transmitted in bytes.",
+		networkLabels, nil)
+
+	networkTxPacketsDesc = prometheus.NewDesc(
+		"ecs_network_transmit_packets_total",
+		"Network packets transmitted.",
+		networkLabels, nil)
+
+	networkTxDroppedDesc = prometheus.NewDesc(
+		"ecs_network_transmit_dropped_total",
+		"Network packets dropped in transmit.",
+		networkLabels, nil)
+
+	networkTxErrorsDesc = prometheus.NewDesc(
+		"ecs_network_transmit_errors_total",
+		"Network errors in transmit.",
+		networkLabels, nil)
+)
+
+var labels = []string{
+	"container",
+}
+
+var cpuLabels = append(
+	labels,
+	"cpu",
+)
+
+var networkLabels = append(
+	labels,
+	"device",
+)
+
+// NewCollector returns a new Collector that queries ECS metadata server
+// for ECS task and container metrics.
+func NewCollector(client *ecsmetadata.Client) prometheus.Collector {
+	return &collector{client: client}
+}
+
+type collector struct {
+	client *ecsmetadata.Client
+}
+
+func (c *collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- cpuTotalDesc
+	ch <- memUsageDesc
+	ch <- memMaxUsageDesc
+	ch <- memLimitDesc
+	ch <- networkRxBytesDesc
+	ch <- networkRxPacketsDesc
+	ch <- networkRxDroppedDesc
+	ch <- networkRxErrorsDesc
+	ch <- networkTxBytesDesc
+	ch <- networkTxPacketsDesc
+	ch <- networkTxDroppedDesc
+	ch <- networkTxErrorsDesc
+}
+
+func (c *collector) Collect(ch chan<- prometheus.Metric) {
+	ctx := context.Background()
+	metadata, err := c.client.RetrieveTaskMetadata(ctx)
+	if err != nil {
+		log.Printf("Failed to retrieve metadata: %v", err)
+		return
+	}
+	stats, err := c.client.RetrieveTaskStats(ctx)
+	if err != nil {
+		log.Printf("Failed to retrieve container stats: %v", err)
+		return
+	}
+	for _, container := range metadata.Containers {
+		s := stats[container.DockerID]
+		if s == nil {
+			log.Printf("Couldn't find container with ID %q in stats", container.DockerID)
+			continue
+		}
+
+		labelVals := []string{
+			container.Name,
+		}
+
+		for i, cpuUsage := range s.CPUStats.CPUUsage.PerCPUUsage {
+			cpu := fmt.Sprintf("%d", i)
+			ch <- prometheus.MustNewConstMetric(
+				cpuTotalDesc,
+				prometheus.CounterValue,
+				cpuJiffiesToSeconds(cpuUsage),
+				append(labelVals, cpu)...,
+			)
+		}
+
+		for desc, value := range map[*prometheus.Desc]float64{
+			memUsageDesc:    s.MemoryStats.Usage,
+			memMaxUsageDesc: s.MemoryStats.MaxUsage,
+			memLimitDesc:    s.MemoryStats.Limit,
+		} {
+			ch <- prometheus.MustNewConstMetric(
+				desc,
+				prometheus.GaugeValue,
+				value,
+				labelVals...,
+			)
+		}
+
+		// Network metrics per inteface.
+		for iface, netStats := range s.Networks {
+			networkLabelVals := append(labelVals, iface)
+
+			for desc, value := range map[*prometheus.Desc]float64{
+				networkRxBytesDesc:   netStats.RxBytes,
+				networkRxPacketsDesc: netStats.RxPackets,
+				networkRxDroppedDesc: netStats.RxDropped,
+				networkRxErrorsDesc:  netStats.RxErrors,
+				networkTxBytesDesc:   netStats.TxBytes,
+				networkTxPacketsDesc: netStats.TxPackets,
+				networkTxDroppedDesc: netStats.TxDropped,
+				networkTxErrorsDesc:  netStats.TxErrors,
+			} {
+				ch <- prometheus.MustNewConstMetric(
+					desc,
+					prometheus.CounterValue,
+					value,
+					networkLabelVals...,
+				)
+			}
+		}
+	}
+}
+
+// cpuJiffiesToSeconds converts CPU metrics
+// in jiffies to seconds.
+func cpuJiffiesToSeconds(j float64) float64 {
+	return j / float64(clockTick)
+}
+
+var clockTick int64 = 100 // Clock ticks are platform dependent, read from system config.
+
+func init() {
+	tick, err := sysconf.Sysconf(sysconf.SC_CLK_TCK)
+	if err != nil {
+		log.Printf("Can't get _SC_CLK_TCK; using 100 instead: %v\n", err)
+		return
+	}
+	log.Printf("sysconf(_SC_CLK_TCK) = %d", tick)
+	clockTick = tick
+}
---
+++ ecsmetadata/client.go
@@ -0,0 +1,198 @@
+// Copyright 2021 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ecsmetadata queries ECS Metadata Server for ECS task metrics.
+// This package is currently experimental and is subject to change.
+package ecsmetadata
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var endpoint string
+
+func init() {
+	const endpointEnv = "ECS_CONTAINER_METADATA_URI_V4"
+	endpoint = os.Getenv(endpointEnv)
+	if endpoint == "" {
+		log.Fatalf("%q environmental variable is not set, are you running this on ECS?", endpointEnv)
+	}
+}
+
+type Client struct {
+	// httpClient is the client to use when making HTTP requests
+	// when set. Otherwise, an internal default client will be used.
+	httpClient *http.Client
+}
+
+func NewClient(httpClient *http.Client) (*Client, error) {
+	if httpClient == nil {
+		httpClient = &http.Client{}
+	}
+	return &Client{httpClient: httpClient}, nil
+}
+
+func (c *Client) RetrieveTaskStats(ctx context.Context) (map[string]*ContainerStats, error) {
+	out := make(map[string]*ContainerStats)
+	err := c.request(ctx, endpoint+"/task/stats", &out)
+	return out, err
+}
+
+func (c *Client) RetrieveTaskMetadata(ctx context.Context) (*TaskMetadata, error) {
+	var out TaskMetadata
+	err := c.request(ctx, endpoint+"/task", &out)
+	return &out, err
+}
+
+func (c *Client) request(ctx context.Context, uri string, out interface{}) error {
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return err
+	}
+	req = req.WithContext(ctx)
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(body, out)
+}
+
+type ContainerStats struct {
+	Name     string  `json:"name"`
+	ID       string  `json:"id"`
+	NumProcs float64 `json:"num_procs"`
+
+	CPUStats struct {
+		CPUUsage struct {
+			TotalUsage        float64   `json:"total_usage"`
+			PerCPUUsage       []float64 `json:"percpu_usage"`
+			UsageInKernelMode float64   `json:"usage_in_kernelmode"`
+			UsageInUserMode   float64   `json:"usage_in_usermode"`
+		} `json:"cpu_usage"`
+		SystemCPUUsage float64 `json:"system_cpu_usage"`
+		OnlineCPUs     float64 `json:"online_cpus"`
+		ThrottlingData struct {
+			Periods          float64 `json:"periods"`
+			ThrottledPeriods float64 `json:"throttled_periods"`
+			ThrottledTime    float64 `json:"throttled_time"`
+		} `json:"throttling_data"`
+	} `json:"cpu_stats"`
+
+	PreCPUStats struct {
+		CPUUsage struct {
+			TotalUsage        float64   `json:"total_usage"`
+			PerCPUUsage       []float64 `json:"percpu_usage"`
+			UsageInKernelMode float64   `json:"usage_in_kernelmode"`
+			UsageInUserMode   float64   `json:"usage_in_usermode"`
+		} `json:"cpu_usage"`
+		SystemCPUUsage float64 `json:"system_cpu_usage"`
+		OnlineCPUs     float64 `json:"online_cpus"`
+		ThrottlingData struct {
+			Periods          float64 `json:"periods"`
+			ThrottledPeriods float64 `json:"throttled_periods"`
+			ThrottledTime    float64 `json:"throttled_time"`
+		} `json:"throttling_data"`
+	} `json:"precpu_stats"`
+
+	MemoryStats struct {
+		Usage    float64 `json:"usage"`
+		MaxUsage float64 `json:"max_usage"`
+		Stats    struct {
+			ActiveAnon              float64 `json:"active_anon"`
+			ActiveFile              float64 `json:"active_file"`
+			Cache                   float64 `json:"cache"`
+			Dirty                   float64 `json:"dirty"`
+			HierarchicalMemoryLimit float64 `json:"hierarchical_memory_limit"`
+			HierarchicalMemswLimit  float64 `json:"hierarchical_memsw_limit"`
+			InactiveAnon            float64 `json:"inactive_anon"`
+			InactiveFile            float64 `json:"inactive_file"`
+			MappedFile              float64 `json:"mapped_file"`
+			Pgfault                 float64 `json:"pgfault"`
+			Pgmajfault              float64 `json:"pgmajfault"`
+			Pgpgin                  float64 `json:"pgpgin"`
+			Pgpgout                 float64 `json:"pgpgout"`
+			RSS                     float64 `json:"rss"`
+			RSSHuge                 float64 `json:"rss_huge"`
+			TotalActiveAnon         float64 `json:"total_active_anon"`
+			TotalActiveFile         float64 `json:"total_active_file"`
+			TotalCache              float64 `json:"total_cache"`
+			TotalDirty              float64 `json:"total_dirty"`
+			TotalInactiveAnon       float64 `json:"total_inactive_anon"`
+			TotalInactiveFile       float64 `json:"total_inactive_file"`
+			TotalMappedFile         float64 `json:"total_mapped_file"`
+			TotalPgfault            float64 `json:"total_pgfault"`
+			TotalPgmajfault         float64 `json:"total_pgmajfault"`
+			TotalPgpgin             float64 `json:"total_pgpgin"`
+			TotalPgpgout            float64 `json:"total_pgpgout"`
+			TotalRSS                float64 `json:"total_rss"`
+			TotalRSSHuge            float64 `json:"total_rss_huge"`
+			TotalUnevictable        float64 `json:"total_unevictable"`
+			TotalWriteback          float64 `json:"total_writeback"`
+			Unevictable             float64 `json:"unevictable"`
+			Writeback               float64 `json:"writeback"`
+		} `json:"stats"`
+		Limit float64 `json:"limit"`
+	} `json:"memory_stats"`
+
+	Networks map[string]struct {
+		RxBytes   float64 `json:"rx_bytes"`
+		RxPackets float64 `json:"rx_packets"`
+		RxErrors  float64 `json:"rx_errors"`
+		RxDropped float64 `json:"rx_dropped"`
+		TxBytes   float64 `json:"tx_bytes"`
+		TxPackets float64 `json:"tx_packets"`
+		TxErrors  float64 `json:"tx_errors"`
+		TxDropped float64 `json:"tx_dropped"`
+	} `json:"networks"`
+
+	NetworkRateStats struct {
+		RxBytesPerSec float64 `json:"rx_bytes_per_sec"`
+		TxBytesPerSec float64 `json:"tx_bytes_per_sec"`
+	} `json:"network_rate_stats"`
+}
+
+// TODO(jbd): Add storage stats.
+
+type TaskMetadata struct {
+	Cluster          string `json:"Cluster"`
+	TaskARN          string `json:"TaskARN"`
+	Family           string `json:"Family"`
+	Revision         string `json:"Revision"`
+	DesiredStatus    string `json:"DesiredStatus"`
+	KnownStatus      string `json:"KnownStatus"`
+	AvailabilityZone string `json:"AvailabilityZone"`
+	LaunchType       string `json:"LaunchType"`
+	Containers       []struct {
+		DockerID      string            `json:"DockerId"`
+		Name          string            `json:"Name"`
+		DockerName    string            `json:"DockerName"`
+		Image         string            `json:"Image"`
+		ImageID       string            `json:"ImageID"`
+		Labels        map[string]string `json:"Labels"`
+		DesiredStatus string            `json:"DesiredStatus"`
+		KnownStatus   string            `json:"KnownStatus"`
+		Type          string            `json:"Type"`
+		ContainerARN  string            `json:"ContainerARN"`
+	} `json:"Containers"`
+}
