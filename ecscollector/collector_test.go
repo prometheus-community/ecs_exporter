a58f363bb3c8af3ee865b70c2b4ce2217bb57e35
Ian Kerins <git@isk.haus>
2025-03-18 04:53:50 -0400

    Add fixture/snapshot tests (#101)
    * Add fixture/snapshot tests
    
    Add tests that transform vendored API responses (fixtures) into vendored
    /metrics output (snapshots), so that we can see how changes to the
    exporter implementation affect the actual output of the exporter, in the
    form of changes to the snapshots.
    
    I've sourced the fixtures from debug JSON logging of real API responses
    from tasks running in AWS. Each task consisted of an ecs_exporter
    container and a prometheus container.
    
    My expectation is that fixtures should not be updated unless we become
    informed that API responses have materially changed, e.g. because new
    features we are using were added, or breaking changes have been made.
    
    I've got two sets of fixtures and snapshots, one for EC2 and one for
    Fargate. The API responses differ between EC2 and Fargate
    enough (because, among other things, they use two completely different
    implementations of the task metadata API, and two different container
    runtimes entirely!) that I think it's worth covering them both
    independently.
    
    Closes #99.
    
    Signed-off-by: Ian Kerins <git@isk.haus>
    
    * Fix typo'd metric name
    
    Compare to the sibling metric whose name is spelled correctly:
    ecs_network_receive_packets_dropped_total.
    
    This commit shows the snapshot tests in action.
    
    Signed-off-by: Ian Kerins <git@isk.haus>
    
    * Fix nil pointer dereference with stopped container in EC2
    
    As the updated task stats fixture for EC2 shows, stopped containers in
    EC2 have the empty JSON object for their stats, instead of the `null`
    that Fargate has, which triggered a nil pointer dereference that is now
    fixed.
    
    Signed-off-by: Ian Kerins <git@isk.haus>
    
    * Load test fixtures at test execution time
    
    Signed-off-by: Ian Kerins <git@isk.haus>
    
    * Generate snapshot diff using testutil.CollectAndCompare
    
    Signed-off-by: Ian Kerins <git@isk.haus>
    
    ---------
    
    Signed-off-by: Ian Kerins <git@isk.haus>

---
+++ ecscollector/collector_test.go
@@ -0,0 +1,140 @@
+// Copyright 2025 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ecscollector
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/prometheus-community/ecs_exporter/ecsmetadata"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+// Create a metadata client that will always receive the given fixture API
+// responses.
+func fixtureClient(taskMetadataPath, taskStatsPath string) (*ecsmetadata.Client, *httptest.Server, error) {
+	taskMetadata, err := os.ReadFile(taskMetadataPath)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read task metadata fixture: %w", err)
+	}
+	taskStats, err := os.ReadFile(taskStatsPath)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read task stats fixture: %w", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /task", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("content-type", "application/json")
+		w.Write(taskMetadata)
+	})
+	mux.HandleFunc("GET /task/stats", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("content-type", "application/json")
+		w.Write(taskStats)
+	})
+
+	server := httptest.NewServer(mux)
+	return ecsmetadata.NewClient(server.URL), server, nil
+}
+
+// Renders metrics from the given collector to the prometheus text exposition
+// format.
+func renderMetrics(collector prometheus.Collector) ([]byte, error) {
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(collector)
+
+	// It seems that the only way to really get full /metrics output is with
+	// promhttp. There is testutil.CollectAndFormat but it requires you to
+	// specify every metric name you want in the output, which seems to be not
+	// worth it compared to this.
+	promServer := httptest.NewServer(promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+	defer promServer.Close()
+	resp, err := http.Get(promServer.URL)
+	if err != nil {
+		return nil, fmt.Errorf("metrics request failed: %w", err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("non-200 metrics response: %v", resp.StatusCode)
+	}
+	metrics, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read metrics response body: %w", err)
+	}
+	return metrics, nil
+}
+
+var updateSnapshots = flag.Bool("update-snapshots", false, "update snapshot files")
+
+func assertSnapshot(t *testing.T, collector prometheus.Collector, path string) {
+	if *updateSnapshots {
+		metrics, err := renderMetrics(collector)
+		if err != nil {
+			t.Fatalf("failed to render new snapshot %s: %v", path, err)
+		}
+		dir := filepath.Dir(path)
+		if err := os.MkdirAll(dir, 0750); err != nil {
+			t.Fatalf("failed to create snapshot output directory %s: %v", dir, err)
+		} else if err := os.WriteFile(path, metrics, 0666); err != nil {
+			t.Fatalf("failed to write snapshot file %s: %v", path, err)
+		} else {
+			t.Logf("updated snapshot: %s", path)
+		}
+	}
+
+	file, err := os.Open(path)
+	if errors.Is(err, os.ErrNotExist) {
+		t.Fatalf("snapshot file does not exist, set the -update-snapshots flag to update: %v", err)
+	} else if err != nil {
+		t.Fatalf("failed to open snapshot file: %v", err)
+	} else if err := testutil.CollectAndCompare(collector, file); err != nil {
+		t.Fatalf("snapshot outdated, set the -update-snapshots flag to update\n%v", err)
+	}
+}
+
+func TestFargateMetrics(t *testing.T) {
+	metadataClient, metadataServer, err := fixtureClient(
+		"testdata/fixtures/fargate_task_metadata.json",
+		"testdata/fixtures/fargate_task_stats.json",
+	)
+	if err != nil {
+		t.Fatalf("failed to load test fixtures: %v", err)
+	}
+	defer metadataServer.Close()
+	collector := NewCollector(metadataClient, slog.Default())
+	assertSnapshot(t, collector, "testdata/snapshots/fargate_metrics.txt")
+}
+
+func TestEc2Metrics(t *testing.T) {
+	metadataClient, metadataServer, err := fixtureClient(
+		"testdata/fixtures/ec2_task_metadata.json",
+		"testdata/fixtures/ec2_task_stats.json",
+	)
+	if err != nil {
+		t.Fatalf("failed to load test fixtures: %v", err)
+	}
+	defer metadataServer.Close()
+	collector := NewCollector(metadataClient, slog.Default())
+	assertSnapshot(t, collector, "testdata/snapshots/ec2_metrics.txt")
+}
